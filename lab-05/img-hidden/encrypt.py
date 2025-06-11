ba5e938f513865c183630b37603301dc967e923b
Nguyen Ngoc Phuoc Sang <phuocsang290524@gmail.com>
2025-06-11 14:30:40 +0700

    [add] lab-05 img-hidden

--- lab-05/img-hidden/encrypt.py
+++ lab-05/img-hidden/encrypt.py
@@ -1,22 +1,44 @@
+import sys
 from PIL import Image
 
-def decode_image(image_path):
+def encode_image(image_path, message):
     img = Image.open(image_path)
     width, height = img.size
 
-    binary_message = ''
+    # Chuyển thông điệp thành chuỗi nhị phân và thêm chuỗi kết thúc
+    binary_message = ''.join(format(ord(char), '08b') for char in message)
+    binary_message += '1111111111111110'  # Dấu hiệu kết thúc thông điệp
+
+    data_index = 0
     for row in range(height):
         for col in range(width):
-            pixel = img.getpixel((col, row))
+            pixel = list(img.getpixel((col, row)))
+
             for color_channel in range(3):
-                binary_message += format(pixel[color_channel], '08b')[-1]
+                if data_index < len(binary_message):
+                    pixel[color_channel] = int(format(pixel[color_channel], '08b')[:-1] + binary_message[data_index], 2)
+                    data_index += 1
 
-    binary_values = [binary_message[i: i+8] for i in range(0, len(binary_message), 8)]
+            img.putpixel((col, row), tuple(pixel))
 
-    message = ''
-    for binary_value in binary_values:
-        if binary_value == '11111111':
+            if data_index >= len(binary_message):
+                break
+        if data_index >= len(binary_message):
             break
-        message += chr(int(binary_value, 2))
 
-    return message
+    # Lưu ảnh sau khi mã hóa thông điệp
+    encoded_image_path = 'encoded_image.png'
+    img.save(encoded_image_path)
+    print("Steganography complete. Encoded image saved as", encoded_image_path)
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python encrypt.py <image_path> <message>")
+        return
+
+    image_path = sys.argv[1]
+    message = sys.argv[2]
+    encode_image(image_path, message)
+
+if __name__ == "__main__":
+    main()
